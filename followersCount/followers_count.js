b0439be6c712a743f3ee024bf433a149275f83db
Hasitha-Nilwakka <hasitha.nilwakka@yahoo.com>
2024-09-26 15:36:58 -0400

    followers count function fies

---
+++ followersCount/followers_count.js
@@ -0,0 +1,18 @@
+let count = 0; //initialize count to 0
+
+function increaseCount(){
+    count++;
+    displayCount(); //display the count
+    checkCountValue(); //check the count and alert
+}
+function checkCountValue(){
+    if (count == 10){
+        alert("Your instagram post has 10 followers. Congradulations!");
+    } else if (count == 20){
+        alert("Your instagram post has 20 followers. Congradulations!");
+    }
+}
+function displayCount(){
+    document.getElementById("countDisplay").innerText = count; //display the count in HTML
+}
+
